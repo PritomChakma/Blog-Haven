d659f6a63a36604362b141b84de0b351847b4b31
pritom <chakmapritom1@gmail.com>
2025-01-23 23:07:45 +0600

    profile updated

--- app/profile/page.js
+++ app/profile/page.js
@@ -5,20 +5,35 @@ import {
 
 const ProfilePage = async () => {
   const session = await getKindeServerSession();
-  const user = await session.getUser();
+  const user = session?.getUser ? await session.getUser() : null;
+
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="bg-gray-100 shadow-lg rounded-lg p-8 max-w-md w-full">
+          <div className="flex flex-col items-center">
+            <h1 className="text-xl font-semibold mb-6">
+              Welcome to your profile!
+            </h1>
+            <p className="mb-4">User information is not available.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="bg-gray-100 shadow-lg rounded-lg p-8 max-w-md w-full">
         <div className="flex flex-col items-center">
           <h1 className="text-xl font-semibold mb-6">
-            Welcome to your profile, {user?.given_name}!
+            Welcome to your profile, {user?.given_name || "Guest"}!
           </h1>
 
           <h1 className="text-2xl font-bold mb-2">
-            {user.given_name} {user?.family_name}
+            {user?.given_name || ""} {user?.family_name || ""}
           </h1>
-          <p className=" mb-4">{user?.email}</p>
+          <p className="mb-4">{user?.email || "Email not available"}</p>
 
           <LogoutLink className="bg-red-500 px-4 py-2 text-white rounded-lg">
             Log out
