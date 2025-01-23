69579c42094f9ec5bd54ae8edc3904be9d4a9a0c
pritom <chakmapritom1@gmail.com>
2025-01-23 23:01:00 +0600

    profile page update

--- app/profile/page.js
+++ app/profile/page.js
@@ -12,13 +12,13 @@ const ProfilePage = async () => {
       <div className="bg-gray-100 shadow-lg rounded-lg p-8 max-w-md w-full">
         <div className="flex flex-col items-center">
           <h1 className="text-xl font-semibold mb-6">
-            Welcome to your profile, {user.given_name}!
+            Welcome to your profile, {user?.given_name}!
           </h1>
 
           <h1 className="text-2xl font-bold mb-2">
-            {user.given_name} {user.family_name}
+            {user.given_name} {user?.family_name}
           </h1>
-          <p className=" mb-4">{user.email}</p>
+          <p className=" mb-4">{user?.email}</p>
 
           <LogoutLink className="bg-red-500 px-4 py-2 text-white rounded-lg">
             Log out
